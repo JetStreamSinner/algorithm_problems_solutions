1eda56ef2b6bd3ff13e7c42bbf81f9b3baf35e16
Jetstream <bigalex934@gmail.com>
2021-08-20 13:41:55 +0300

    Init commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+template<typename T>
+void checkTestCase(const T &received, const T &expected, const std::string &description)
+{
+    std::cout << "[Status: ";
+    if (expected == received)
+        std::cout << "Success] ";
+    else
+        std::cout << "Failed] ";
+    std::cout << "[Expected: " << expected << ". Received: " << received << "] ";
+    std::cout << "[Description: " << description << "]" << std::endl;
+}
+
+int main(int argc, char * argv[])
+{
+    return 0;
+}
