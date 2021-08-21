61031617e78daa284eb18e45d7b4d5f3146c4b06
Alex <bigalex934@gmail.com>
2021-08-21 14:37:42 +0300

    Solution for Find First And Last Position Of Element in Sorted Array

--- find_first_and_last_position.h
+++ find_first_and_last_position.h
@@ -1,4 +1,102 @@
+
 #ifndef FIND_FIRST_AND_LAST_POSITION_H
 #define FIND_FIRST_AND_LAST_POSITION_H
+
+#include <vector>
+#include <algorithm>
+
+// Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+// If target is not found in the array, return [-1, -1].
+// You must write an algorithm with O(log n) runtime complexity.
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+template<typename T>
+bool operator==(std::vector<T> &vec1, const std::vector<T> &vec2)
+{
+    if (vec1.size() != vec2.size())
+        return false;
+
+    auto firstVecIterator = vec1.cbegin();
+    auto secondVecIterator = vec2.cbegin();
+
+    const auto firstVecEnd = vec1.cend();
+
+    while (firstVecIterator != firstVecEnd) {
+        if (*firstVecIterator != *secondVecIterator)
+            return false;
+        ++firstVecIterator;
+        ++secondVecIterator;
+    }
+    return true;
+}
+
+template<typename T>
+std::ostream& operator<<(std::ostream &os, const std::vector<T> &vec)
+{
+    os << "{ ";
+    for (const auto &value : vec)
+        os << value << " ";
+    os << "}";
+    return os;
+}
+
+class FindFirstAndLastPosition
+{
+public:
+    template<typename Container>
+    static std::vector<int> searchRange(Container &&nums, int target)
+    {
+        if (nums.empty())
+            return { -1, -1 };
+
+        const auto startIterator = nums.begin();
+        const auto endIterator = nums.end();
+
+        auto [rangeStartFrom, status] = binarySearch(nums, target);
+
+        if (!status)
+            return { -1, -1 };
+
+        auto leftForwarding = rangeStartFrom;
+        auto rightForwarding = rangeStartFrom;
+
+        while (leftForwarding != startIterator && *std::prev(leftForwarding) == *rangeStartFrom)
+            --leftForwarding;
+
+        while (rightForwarding != std::prev(endIterator) && *std::next(rightForwarding) == *rangeStartFrom)
+            ++rightForwarding;
+
+        auto startIndex = static_cast<int>(std::distance(startIterator, leftForwarding));
+        auto endIndex = static_cast<int>(std::distance(startIterator, rightForwarding));
+
+        return { startIndex, endIndex };
+    }
+
+    using It = std::vector<int>::iterator;
+    template<typename Container>
+    static std::pair<It, bool> binarySearch(Container &&container, int target)
+    {
+        return binarySearchImpl(container.begin(), container.end(), target);
+    }
+
+    static std::pair<It, bool> binarySearchImpl(It rangeStart, It rangeEnd, int target)
+    {
+        const auto rangeLength = std::distance(rangeStart, rangeEnd);
+        const auto middleItem = std::next(rangeStart, rangeLength / 2);
+
+        if (rangeLength == 1 && *middleItem != target)
+            return { It(), false };
+
+        if (*middleItem == target)
+            return { middleItem, true };
+
+        if (*middleItem > target)
+            return binarySearchImpl(rangeStart, middleItem, target);
+        else if (*middleItem < target)
+            return binarySearchImpl(middleItem, rangeEnd, target);
+
+        return { It(), false };
+    }
+};
 
 #endif //FIND_FIRST_AND_LAST_POSITION_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "find_first_and_last_position.h"
 
 template<typename T>
 void checkTestCase(const T &received, const T &expected, const std::string &description)
@@ -14,5 +15,13 @@ void checkTestCase(const T &received, const T &expected, const std::string &desc
 
 int main(int argc, char * argv[])
 {
+    checkTestCase(FindFirstAndLastPosition::searchRange(std::vector<int>({5, 7, 7, 8, 8, 10}), 8), std::vector<int>{3, 4}, "Sample case");
+    checkTestCase(FindFirstAndLastPosition::searchRange(std::vector<int>({5, 7, 7, 8, 8, 10}), 6), std::vector<int>{-1, -1}, "Sample case");
+    checkTestCase(FindFirstAndLastPosition::searchRange(std::vector<int>({1, 3}), 1), std::vector<int>{0, 0}, "Sample case");
+    checkTestCase(FindFirstAndLastPosition::searchRange(std::vector<int>({ 1 }), 1), std::vector<int>{0, 0}, "Sample case");
+    checkTestCase(FindFirstAndLastPosition::searchRange(std::vector<int>({}), 0), std::vector<int>{-1, -1}, "Sample case");
+    checkTestCase(FindFirstAndLastPosition::searchRange(std::vector<int>({ 1 }), 0), std::vector<int>{-1, -1}, "Sample case");
+    checkTestCase(FindFirstAndLastPosition::searchRange(std::vector<int>({ 1, 3 }), 1), std::vector<int>{ 0, 0 }, "Sample case");
+
     return 0;
 }
