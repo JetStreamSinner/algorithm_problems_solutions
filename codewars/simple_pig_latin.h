f74d798efc2dadf920610640740fc01ee492668a
Alex <bigalex934@gmail.com>
2021-12-24 02:03:34 +0300

    Implement solution for simple pig latin. Also split directories for codewars and leetcode

---
+++ codewars/simple_pig_latin.h
@@ -0,0 +1,69 @@
+#pragma once
+
+// https://www.codewars.com/kata/520b9d2ad5c005041100000f
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+#include <cctype>
+#include "../utils/pretty_print.h"
+
+class Solution
+{
+
+public:
+    std::string pig_it(const std::string &str)
+    {
+        const auto tokens = tokenize(str);
+
+        std::string result;
+        const auto suffix = "ay";
+
+        auto forwarder = tokens.begin();
+        const auto endIterator = tokens.end();
+        for (; forwarder != endIterator; ++forwarder) {
+
+            bool currentLast = std::next(forwarder) == endIterator;
+            std::string ender = (currentLast ? "" : " ");
+
+            bool singleCharacter = forwarder->size() == 1;
+
+            if (singleCharacter) {
+                bool punctCharacter = std::ispunct(forwarder->at(0));
+
+                if (punctCharacter)
+                    result += *forwarder + ender;
+                else
+                    result += *forwarder + suffix + ender;
+
+                continue;
+            }
+
+            const auto start = forwarder->begin();
+            const auto next = std::next(start);
+            const auto end = forwarder->end();
+
+            std::string firstCharacter(start, next);
+            std::string appendable(next, end);
+
+            result += appendable + *start + suffix + ender;
+        }
+        return result;
+    }
+
+    std::vector<std::string> tokenize(const std::string &source)
+    {
+        std::stringstream ss(source);
+        std::istream_iterator<std::string> forwarder(ss);
+        std::istream_iterator<std::string> endIterator;
+
+        std::vector<std::string> tokens;
+        std::copy(forwarder, endIterator, std::back_inserter(tokens));
+        return std::move(tokens);
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
+#include <string>
 #include "utils/pretty_print.h"
-#include "combination_sum.h"
+#include "codewars/simple_pig_latin.h"
 
-template<typename T>
-void checkTestCase(const T &received, const T &expected, const std::string &description)
+template<typename T = std::basic_string<char>, typename DescrT>
+void checkTestCase(T&& received, T&& expected, DescrT&& description)
 {
     std::cout << "[Status: ";
     if (expected == received)
         std::cout << "Success] ";
     else
         std::cout << "Failed] ";
-    std::cout << "[Expected: " << expected << ". Received: " << received << "] ";
+    std::cout << "[Expected |" << expected << "| Received |" << received << "| ] ";
     std::cout << "[Description: " << description << "]" << std::endl;
 }
 
 int main(int argc, char * argv[])
 {
+    checkTestCase(std::string(Solution().pig_it("This is my string")), std::string("hisTay siay ymay tringsay"), "Test case 1");
+    checkTestCase(std::string(Solution().pig_it("Pig latin is cool")), std::string("igPay atinlay siay oolcay"), "Test case 2");
+    checkTestCase(std::string(Solution().pig_it("Acta est fabula")), std::string("ctaAay steay abulafay"), "Test case 3");
+    checkTestCase(std::string(Solution().pig_it("Hello world !")), std::string("elloHay orldway !"), "Test case 4");
     return 0;
 }
