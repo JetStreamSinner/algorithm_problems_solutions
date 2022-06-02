abd286b1823ca0d52760e94435f3fb0d5fcb52f4
Jetstream <bigalex934@gmail.com>
2022-06-02 20:52:46 +0300

    Implement custom solution for move_zeroes

--- C++/leetcode/move_zeroes.h
+++ C++/leetcode/move_zeroes.h
@@ -10,12 +10,31 @@
 class Solution {
 public:
 
+    void customWayImplementation(std::vector<int> &nums) {
+        const auto end_iterator = nums.end();
+        auto result_forwarder = nums.begin();
+        auto forwarder = nums.begin();
+
+        while (forwarder != end_iterator) {
+            auto next_non_zero = std::find_if_not(forwarder, end_iterator, [](auto value) {
+                return value == 0;
+            });
+
+            if (next_non_zero == end_iterator)
+                break;
+
+            std::swap(*next_non_zero, *result_forwarder);
+            forwarder = std::next(next_non_zero);
+            result_forwarder = std::next(result_forwarder);
+        }
+    }
+
     void stlWayImplementation(std::vector<int> &nums) {
         auto zeroes_from = std::remove(nums.begin(), nums.end(), 0);
         std::fill(zeroes_from, nums.end(), 0);
     }
 
     void moveZeroes(std::vector<int> &nums) {
-        return stlWayImplementation(nums);
+        return customWayImplementation(nums);
     }
 };
