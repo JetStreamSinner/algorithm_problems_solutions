ea307567dbd703170d5a29e358b4a2793fc79a46
Jetstream <bigalex934@gmail.com>
2022-09-13 00:19:46 +0300

    Implement binary search approach solution for "Sqrt(x)"

--- C++/leetcode/sqrt_x.h
+++ C++/leetcode/sqrt_x.h
@@ -1,16 +1,31 @@
 #pragma once
 
-
 class Solution {
 public:
     int mySqrt(int number) {
-        for (std::size_t value = 0; value <= number / 2 + 1; ++value) {
-            if (value * value == number) {
-                return value;
-            } else if (value * value > number) {
-                return value - 1;
+        return binary_search(number);
+    }
+
+private:
+
+    long long int binary_search(int target) {
+        long long int lower = 0;
+        long long int upper = target;
+        long long int middle = lower + (upper - lower) / 2;
+        long long int result = -1;
+
+        while (lower <= upper) {
+            long long int square = middle * middle;
+            if (square == target) {
+                return middle;
+            } else if (square < target) {
+                result = middle;
+                lower = middle + 1;
+            } else {
+                upper = middle - 1;
             }
+            middle = lower + (upper - lower) / 2;
         }
-        return -1;
+        return result;
     }
 };
