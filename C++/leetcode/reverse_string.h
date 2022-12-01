d7ced14f5866aaed06e8d3f83d3f50308275c324
Jetstream <bigalex934@gmail.com>
2022-12-01 21:03:56 +0300

    Implement stl way approach for "Reverse string"

---
+++ C++/leetcode/reverse_string.h
@@ -0,0 +1,10 @@
+#pragma once
+// Write a function that reverses a string. The input string is given as an array of characters s.
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+class Solution {
+public:
+    void reverseString(std::vector<char>& s) {
+        std::reverse(s.begin(), s.end());
+    }
+};
