7e916e4873b483682ffbae7671c198e5ca480706
Jetstream <bigalex934@gmail.com>
2022-12-17 16:40:56 +0300

    Implemented solution for "Binary search"

---
+++ C++/leetcode/binary_search.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// https://leetcode.com/problems/binary-search/submissions/
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function
+// to search target in nums. If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        auto it = std::lower_bound(nums.begin(), nums.end(), target);
+        if (it == nums.end() || *it != target)
+            return -1;
+        else
+            return std::distance(nums.begin(), it);
+    }
+};
