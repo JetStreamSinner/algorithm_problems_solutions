6844ba2dd2725496080cb2b72ecb8492ac5be10a
Script <bigalex934@gmail.com>
2023-12-16 19:15:11 +0300

    Implement roman_decoder

--- Rust/rust_algo_solutions/src/codewars/roman_decoder.rs
+++ Rust/rust_algo_solutions/src/codewars/roman_decoder.rs
@@ -1,5 +1,47 @@
 pub fn decode(roman: &str) -> u64 {
-    0
+
+    let roman_digits: Vec<i32> = roman.chars().rev().map(|roman_digit| map_to_arabic(roman_digit)).collect();
+
+    if roman_digits.is_empty() {
+        return 0;
+    }
+
+    if roman_digits.len() == 1 {
+        return roman_digits[0] as u64;
+    }
+
+    let mut accumulator: u64 = 0;
+
+
+    let mut index: usize = 0;
+    while index < roman_digits.len() {
+        let curr = roman_digits.get(index).unwrap();
+        let next = roman_digits.get(index + 1).unwrap_or(curr);
+        let next_digit_great_or_eq = next - curr >= 0;
+
+        if next_digit_great_or_eq {
+            accumulator += *curr as u64;
+            index += 1;
+        } else {
+            accumulator += (curr - next) as u64;
+            index += 2;
+        }
+    }
+
+    accumulator
+}
+
+fn map_to_arabic(roman_digit: char) -> i32 {
+    match roman_digit {
+        'M' => 1000,
+        'D' => 500,
+        'C' => 100,
+        'L' => 50,
+        'X' => 10,
+        'V' => 5,
+        'I' => 1,
+        _ => panic!("Unknown roman digit"),
+    }
 }
 
 #[cfg(test)]
@@ -7,9 +49,13 @@ mod roman_decoder_test {
     use super::decode;
 
     fn test_equal(input: &str, actual: u64, expected: u64) {
-        assert_eq!(actual, expected, "\nYour result (left) did not match the expected output (right) for the input \"{}\"", input);
+        assert_eq!(
+            actual, expected,
+            "\nYour result (left) did not match the expected output (right) for the input \"{}\"",
+            input
+        );
     }
-    
+
     #[test]
     fn basic() {
         test_equal("", decode(""), 0);
@@ -18,7 +64,7 @@ mod roman_decoder_test {
         test_equal("MMVIII", decode("MMVIII"), 2008);
         test_equal("MDCLXVI", decode("MDCLXVI"), 1666);
     }
-    
+
     #[test]
     fn one_through_ten() {
         test_equal("I", decode("I"), 1);
@@ -32,7 +78,7 @@ mod roman_decoder_test {
         test_equal("IX", decode("IX"), 9);
         test_equal("X", decode("X"), 10);
     }
-    
+
     #[test]
     fn big_numbers() {
         test_equal("C", decode("C"), 100);
@@ -45,5 +91,5 @@ mod roman_decoder_test {
         test_equal("MMM", decode("MMM"), 3000);
         test_equal("MMMCM", decode("MMMCM"), 3900);
         test_equal("MMMCMXIV", decode("MMMCMXIV"), 3914);
-    }    
+    }
 }
