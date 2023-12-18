22ef2dd9e4f5a8aa47c4e897336728d9a56b19e8
Script <bigalex934@gmail.com>
2023-12-18 09:04:15 +0300

    Implement unique_in_order

--- Rust/rust_algo_solutions/src/codewars.rs
+++ Rust/rust_algo_solutions/src/codewars.rs
@@ -1,9 +1,20 @@
 mod roman_decoder;
+mod unique_in_order;
 
 mod codewars {
     use super::roman_decoder;
+    use super::unique_in_order;
+    use super::unique_in_order::unique_in_order_impl;
 
-    fn roman_as_num(roman: &str) -> u64 {
+    pub fn roman_as_num(roman: &str) -> u64 {
         roman_decoder::decode(roman)
+    }
+
+    pub fn unique_in_order<T>(sequence: T) -> Vec<T::Item>
+    where
+        T: std::iter::IntoIterator,
+        T::Item: std::cmp::PartialEq + std::fmt::Debug + Copy,
+    {
+        unique_in_order_impl(sequence)
     }
 }
---
+++ Rust/rust_algo_solutions/src/codewars/unique_in_order.rs
@@ -0,0 +1,23 @@
+
+pub fn unique_in_order_impl<T>(sequence: T) -> Vec<T::Item>
+where
+    T: std::iter::IntoIterator,
+    T::Item: std::cmp::PartialEq + std::fmt::Debug,
+{
+    let mut input: Vec<T::Item> = sequence.into_iter().collect();
+    input.dedup();
+    input
+}
+
+#[cfg(test)]
+mod unique_in_order_test {
+    use super::*;
+
+    #[test]
+    fn sample_test() {
+        assert_eq!(
+            unique_in_order_impl("AAAABBBCCDAABBB".chars()),
+            vec!['A', 'B', 'C', 'D', 'A', 'B']
+        );
+    }
+}
